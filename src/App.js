41edaa98f6e9cf50efe3e3bedb7a1c467bd8bb99
Jeremy Mark <jermark851@gmail.com>
2020-02-03 22:14:21 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-//parent import
 import React, { Component } from 'react';
-//child imports
 import './index.css';
-import TopicBrowser from './components/TopicBrowser/TopicBrowser';
+// import TopicBrowser from './components/TopicBrowser/TopicBrowser';
 
 class App extends Component {
   render() {
     return (
-      <TopicBrowser />
+      // <TopicBrowser />
     )
   }
 }
 
-//export
 export default App;
