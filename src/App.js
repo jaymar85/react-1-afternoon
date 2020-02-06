df5bf0605de3055f700105c2c6c6389f08b8d020
Jeremy Mark <jermark851@gmail.com>
2020-02-05 18:12:04 -0600

    completed evenandodds

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './index.css';
-// import TopicBrowser from './components/TopicBrowser/TopicBrowser';
+import TopicBrowser from '../src/components/TopicBrowser/TopicBrowser';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Hello World</p>
+        <TopicBrowser/>
       </div>
     )
   }
